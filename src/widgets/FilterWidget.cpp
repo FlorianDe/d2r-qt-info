25723f5ebe60495e886bd4b0fef785e9d08f46f6
FlorianDe <FlorianDe@users.noreply.github.com>
2024-10-09 13:10:15 +0200

    Integrated the base type images inside the filter by customizing the QCombobox rendering

--- src/widgets/FilterWidget.cpp
+++ src/widgets/FilterWidget.cpp
@@ -2,28 +2,88 @@
 #include "RuneCheckBoxGridWidget.h"
 #include "SocketAmountCheckBoxGridWidget.h"
 
-#include <QPointer>
+#include <QApplication>
+#include <QComboBox>
+#include <QPainter>
+#include <QStandardItem>
+#include <QStyledItemDelegate>
+
 #include <QGroupBox>
+#include <QPointer>
+
+struct BaseItemTypeComboBoxModel {
+	QIcon image;		 // NOLINT(*-identifier-naming)
+	int indentation; // NOLINT(*-identifier-naming)
+};
+class BaseItemTypeComboBoxDelegate : public QStyledItemDelegate {
+public:
+	BaseItemTypeComboBoxDelegate(QObject* parent = nullptr) : QStyledItemDelegate(parent) {}
+
+	void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const override {
+		painter->save();
+
+		const auto extraDataVariant = index.data(Qt::UserRole);
+		const auto extraData = qvariant_cast<BaseItemTypeComboBoxModel>(extraDataVariant);
+		QString text = index.data(Qt::DisplayRole).toString();
+
+		// Draw the background
+		painter->fillRect(option.rect,
+											option.state & QStyle::State_Selected ? option.palette.highlight() : option.palette.window());
+
+		// Draw the icon
+		if (!extraDataVariant.isValid()) {
+			qDebug() << "QVariant data is invalid";
+		} else {
+			if (!extraData.image.isNull()) {
+				QPixmap pixmap = extraData.image.pixmap(12, 12);
+				QRect iconRect(option.rect.x() + 5 + extraData.indentation * 20,
+											 option.rect.y() + (option.rect.height() - pixmap.height()) / 2, pixmap.width(), pixmap.height());
+				painter->drawPixmap(iconRect, pixmap);
+			} else {
+				qDebug() << "Icon not found for QCombobox:" << text;
+			}
+		}
+
+		// Calculate the indentation for the text
+		QRect textRect(option.rect.x() + 35 + extraData.indentation * 20, option.rect.y(), option.rect.width() - 25,
+									 option.rect.height());
+		painter->drawText(textRect, Qt::AlignVCenter | Qt::TextWordWrap, text);
+
+		painter->restore();
+	}
+
+	QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const override {
+		return QSize(option.rect.width(), 32);
+	}
+};
 
 FilterWidget::FilterWidget(QWidget* parent) : QWidget(parent) { setupUI(); }
 
 void FilterWidget::setupUI() {
 	const QPointer mainLayout = new QVBoxLayout();
 
-	QLabel* runeWordLabel = new QLabel(tr("Select runeword")+":", this);
+	QLabel* runeWordLabel = new QLabel(tr("Select runeword") + ":", this);
 	m_runeWordComboBox = new QComboBox(this);
 	this->connect(m_runeWordComboBox, QOverload<int>::of(&QComboBox::currentIndexChanged), this,
-					&FilterWidget::onRuneWordSelected);
+								&FilterWidget::onRuneWordSelected);
 
 	// Create a horizontal line as a divider
 	QFrame* line = new QFrame(this);
 	line->setFrameShape(QFrame::HLine);		// Horizontal line
 	line->setFrameShadow(QFrame::Sunken); // Sunken style
 
-	QLabel* itemTypeLabel = new QLabel(tr("Select item type")+":", this);
+	QLabel* itemTypeLabel = new QLabel(tr("Select item type") + ":", this);
 
 	m_itemTypeComboBox = new QComboBox(this);
-	populateItemTypes();
+	m_itemTypeComboBox->setItemDelegate(new BaseItemTypeComboBoxDelegate(this));
+	QStringList items = {"Weapons", "Armors", "Shields", "Axes"};
+	for (int i = 0; i < items.size(); ++i) {
+		const auto iconPath = ":/images/item_base_types/" + items[i].toLower() + ".png";
+		auto image = QIcon(iconPath);
+		BaseItemTypeComboBoxModel extraData{image, 0};
+		m_itemTypeComboBox->addItem(items[i], QVariant::fromValue(extraData));
+	}
+	// populateItemTypes();
 
 	QVBoxLayout* socketSelectionLayout = new QVBoxLayout();
 	QLabel* socketsSelectionLabel = new QLabel(tr("Sockets"));
@@ -51,7 +111,6 @@ void FilterWidget::setupUI() {
 
 	mainLayout->addLayout(buttonLayout);
 
-
 	this->setLayout(mainLayout);
 }
 
@@ -77,19 +136,18 @@ FilterWidget::FilterState FilterWidget::getFilterState() const {
 	const QString itemType = m_itemTypeComboBox->currentIndex() == -1 ? nullptr : m_itemTypeComboBox->currentText();
 	const QVector<int> selectedSocketOptions = m_socketsSelection->getSelectedSocketAmounts();
 	const QVector<QString> selectedRunes = m_runeCheckBoxWidget->getSelectedRunes();
-	const RuneCheckBoxGridWidget::LogicalRunesOperator logicalRunesOperator = m_runeCheckBoxWidget->getSelectedRunesOperator();
+	const RuneCheckBoxGridWidget::LogicalRunesOperator logicalRunesOperator =
+			m_runeCheckBoxWidget->getSelectedRunesOperator();
 
 	return {
-		itemType,
-		selectedSocketOptions,
-		selectedRunes,
-		logicalRunesOperator,
+			itemType,
+			selectedSocketOptions,
+			selectedRunes,
+			logicalRunesOperator,
 	};
 }
 
-void FilterWidget::onSearchClicked() {
-	emit filterChanged(this->getFilterState());
-}
+void FilterWidget::onSearchClicked() { emit filterChanged(this->getFilterState()); }
 
 void FilterWidget::onResetClicked() {
 	m_runeWordComboBox->setCurrentIndex(-1);
@@ -97,9 +155,6 @@ void FilterWidget::onResetClicked() {
 	m_socketsSelection->reset();
 	m_runeCheckBoxWidget->reset();
 
-	emit filterChanged({
-	nullptr,
-		m_socketsSelection->getSelectedSocketAmounts(),
-		m_runeCheckBoxWidget->getSelectedRunes()
-	});
+	emit filterChanged(
+			{nullptr, m_socketsSelection->getSelectedSocketAmounts(), m_runeCheckBoxWidget->getSelectedRunes()});
 }
